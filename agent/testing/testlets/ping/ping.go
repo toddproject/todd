e9f72e82b1d015d72cb41cc234ad773afa9b52e2
Matt Oswalt <matt@keepingitclassless.net>
2016-07-29 02:21:01 -0700

    Implement async handling of testlet running
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- agent/testing/testlets/ping/ping.go
+++ agent/testing/testlets/ping/ping.go
@@ -9,25 +9,38 @@ package toddping
 // Currently, each testlet is stored within the ToDD repo in order to
 // vet out the architecture, which means they are awkwardly placed
 // underneath the "testlets" directory together. However, this should be
-// a tempoarary holding place, as the main effort around native testlets
+// a temporary holding place, as the main effort around native testlets
 // is being implemented so they can be broken out into their own repos.
 
 import (
 	"time"
 
+	// log "github.com/Sirupsen/logrus"
+
 	"github.com/Mierdin/todd/agent/testing/testlets"
 )
 
-type PingTestlet struct{}
+type PingTestlet struct {
+	testlets.BaseTestlet
+}
 
 func init() {
 
+	var pt = PingTestlet{}
+
+	// Ensure the RunFunction attribute is set correctly.
+	// This allows the underlying testlet infrastructure
+	// to know what function to call at runtime
+	pt.RunFunction = pt.RunTestlet
+
 	// This is important - register the name of this testlet
 	// (the name the user will use in a testrun definition)
-	testlets.Register("ping", &PingTestlet{})
+	testlets.Register("ping", &pt)
 }
 
-func (p PingTestlet) Run(target string, args []string) (map[string]string, error) {
+// RunTestlet implements the core logic of the testlet. Don't worry about running asynchronously,
+// that's handled by the infrastructure.
+func (p PingTestlet) RunTestlet(target string, args []string) (map[string]string, error) {
 
 	// TODO(mierdin): Implement ping test here - this is just a mock
 	time.Sleep(3000 * time.Millisecond)
@@ -36,12 +49,4 @@ func (p PingTestlet) Run(target string, args []string) (map[string]string, error
 		"packet_loss_percentage": "0",
 	}, nil
 
-}
-
-func (p PingTestlet) Kill() error {
-	// TODO (mierdin): This will have to be coordinated with the task above. Basically
-	// you need a way to kill this testlet (and that's really only possible when running
-	// async)
-
-	return nil
 }
