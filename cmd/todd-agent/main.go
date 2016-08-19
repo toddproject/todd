4929bec6d46d5586b945ef8b1db7c2031d039d0d
Matt Oswalt <matt@keepingitclassless.net>
2016-08-19 14:52:22 -0700

    Got native testlets working
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- cmd/todd-agent/main.go
+++ cmd/todd-agent/main.go
@@ -23,11 +23,11 @@ import (
 	"github.com/Mierdin/todd/comms"
 	"github.com/Mierdin/todd/config"
 	"github.com/Mierdin/todd/hostresources"
-
 	// Testlet imports (importing these packages registers the testlets)
 	// Need to make this dynamic if possible (at compile time of course)
 	// TODO Not necessary anymore
 	//_ "github.com/toddproject/todd-nativetestlet-ping/testlet"
+)
 
 // Command-line Arguments
 var arg_config string
